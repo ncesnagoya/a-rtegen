03590b9de4fa59477386f6785c095c5f851495e3
nces-mtakada <nces-mtakada@localhost>
2017-10-20 09:05:59 +0000

    a-rtegten Release 1.2.1をコミット

--- src/jp.ac.nagoya_u.is.nces.a_rte.model/src/jp/ac/nagoya_u/is/nces/a_rte/model/ar4x/instance/RVariableDataInstanceInSwc.java
+++ src/jp.ac.nagoya_u.is.nces.a_rte.model/src/jp/ac/nagoya_u/is/nces/a_rte/model/ar4x/instance/RVariableDataInstanceInSwc.java
@@ -2,7 +2,7 @@
  *  TOPPERS/A-RTEGEN
  *      Automotive Runtime Environment Generator
  *
- *  Copyright (C) 2013-2014 by Eiwa System Management, Inc., JAPAN
+ *  Copyright (C) 2013-2015 by Eiwa System Management, Inc., JAPAN
  *
  *  上記著作権者は，以下の(1)〜(4)の条件を満たす場合に限り，本ソフトウェ
  *  ア（本ソフトウェアを改変したものを含む．以下同じ）を使用・複製・改
@@ -159,8 +159,12 @@ public interface RVariableDataInstanceInSwc extends VariableDataInstanceInSwc {
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * QueuedReceiverComSpecのキュー長を返す
+	 * NOTE: ComSpecがないときは、nullU問題の対応で1を返す（暫定対応となるため、モデル違反として扱うときには、nullを返すように戻すこと
+	 * <!-- end-model-doc -->
 	 * @model kind="operation" dataType="jp.ac.nagoya_u.is.nces.a_rte.model.ar4x.m2.PositiveInteger" required="true"
-	 *        annotation="http://www.eclipse.org/emf/2002/Ecore/OCL/Pivot body='let comSpec : m2::QueuedReceiverComSpec = getComSpec().oclAsType(m2::QueuedReceiverComSpec)\n\t\t\t\tin if comSpec.oclIsUndefined()\n\t\t\t\t\tthen null\n\t\t\t\t\telse comSpec.queueLength\n\t\t\t\t\tendif'"
+	 *        annotation="http://www.eclipse.org/emf/2002/Ecore/OCL/Pivot body='let comSpec : m2::QueuedReceiverComSpec = getComSpec().oclAsType(m2::QueuedReceiverComSpec)\n\t\t\t\tin if comSpec.oclIsUndefined()\n\t\t\t\t\tthen 1\n\t\t\t\t\telse comSpec.queueLength\n\t\t\t\t\tendif'"
 	 * @generated
 	 */
 	Integer getQueueLength();
